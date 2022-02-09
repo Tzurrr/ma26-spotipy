e84d24977a67d3675954843f2db4cb09d354b276
Tzurrr <zur001@gmail.com>
2022-02-09 14:18:16 +0200

    changed the logic for track class and made it inner class of the album class

--- music/track.py
+++ music/track.py
@@ -4,7 +4,7 @@ from exeptions.spotipy_exeptions import *
 class track:
     track = {}
     def __init__(self, track: dict):
-        if track != {}:
+        if self.track != {}:
             raise CantInsertTwoTracksIntoOneExeption("Cant Insert Two Tracks Into One")
         else:
             self.track = track
