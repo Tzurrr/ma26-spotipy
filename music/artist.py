27ed0d83ce27286023e1c99b781603265d032cbd
Tzurrr <zur001@gmail.com>
2022-02-09 18:56:52 +0200

    removed 'get_albums' def

--- music/artist.py
+++ music/artist.py
@@ -2,18 +2,15 @@ import copy
 from music.album import Album
 class artist:
     artist_data = {}
+    used = []
     albums = []
-    def __init__(self, artist: dict, overalldata:dict):
-        self.artist_data["artist_id"] = artist.get("id")
-        self.artist_data["artist_name"] = artist.get("name")
+    def __init__(self, overalldata:dict, ord):
+        self.artist_data["artist_id"] = overalldata.get('track').get("artists")[ord].get("id")
+        self.artist_data["artist_name"] = overalldata.get('track').get("artists")[ord].get("name")
 
-        temp_album = copy.deepcopy(overalldata)
-        temp_album.get("track").pop("artists")
 
-        if Album(temp_album) in self.albums:
-            pass
-        else:
-            self.albums.append(Album(temp_album))
+        overalldata.get("track").pop("artists")
+        if overalldata not in self.used:
+            self.used.append(overalldata)
+            self.albums.append(Album(copy.deepcopy(overalldata)))
 
-    def get_albums(self):
-        return self.albums
