19bd52c77da64a6709f6ccbad45f2b237df7434d
Tzurrr <zur001@gmail.com>
2022-02-09 16:50:09 +0200

    removed the class and made a generator function instead

--- read/reader.py
+++ read/reader.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import json
+import copy
 
 from music.artist import artist
 
@@ -9,16 +10,13 @@ with open(r"D:\Code\Spotipy\config.yaml") as a_yaml_file:
     path = parsed_yaml_file["SONG_DIRECTORY_PATH"].get("PATH")
 
 
-class reader:
-    available_artists = []
-    def read(self, some_key_to_filter_on = None):
-        for file in os.listdir(path):
-            f = os.path.join(path, file)
-            with open(f, "r") as song:
-                temp_songs_dict = json.load(song)
-                for artist_data in temp_songs_dict.get('track').get("artists"):
-                    self.available_artists.append(artist(artist_data, temp_songs_dict))
-
-
-reader = reader()
-reader.read()
+#class reader:
+#    available_artists = []
+#    counter = 0
+def read(some_key_to_filter_on=None):
+    for file in os.listdir(path):
+        f = os.path.join(path, file)
+        with open(f, "r") as song:
+            temp_songs_dict = json.load(song)
+            for artist_data in temp_songs_dict.get('track').get("artists"):
+                yield artist(artist_data, temp_songs_dict)
