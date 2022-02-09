338ffa5e1cd93d9dd4eb2382f5756b2b811a38bf
Tzurrr <zur001@gmail.com>
2022-02-09 14:19:49 +0200

    added a reader class which reads all the songs files and makes objects from them

--- read/reader.py
+++ read/reader.py
@@ -1,11 +1,24 @@
 import os
+import yaml
+import json
+
+from music.artist import artist
+
+with open(r"D:\Code\Spotipy\config.yaml") as a_yaml_file:
+    parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
+    path = parsed_yaml_file["SONG_DIRECTORY_PATH"].get("PATH")
+
 
 class reader:
-    def read(self):
-        #songs_directory =
-        with open(r"D:\Code\Spotipy\songs", "r") as songs_folder:
-            for song in songs_folder:
-                print(song)
+    available_artists = []
+    def read(self, some_key_to_filter_on = None):
+        for file in os.listdir(path):
+            f = os.path.join(path, file)
+            with open(f, "r") as song:
+                temp_songs_dict = json.load(song)
+                for artist_data in temp_songs_dict.get('track').get("artists"):
+                    self.available_artists.append(artist(artist_data, temp_songs_dict))
+
 
 reader = reader()
 reader.read()
