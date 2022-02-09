57d92a3729fdc57767789a22e5bebd6b571a1c99
Tzurrr <zur001@gmail.com>
2022-02-09 19:01:33 +0200

    changed a bit of the def's logic

--- read/reader.py
+++ read/reader.py
@@ -9,14 +9,15 @@ with open(r"D:\Code\Spotipy\config.yaml") as a_yaml_file:
     parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
     path = parsed_yaml_file["SONG_DIRECTORY_PATH"].get("PATH")
 
-
-#class reader:
-#    available_artists = []
-#    counter = 0
 def read(some_key_to_filter_on=None):
     for file in os.listdir(path):
         f = os.path.join(path, file)
         with open(f, "r") as song:
             temp_songs_dict = json.load(song)
+            count = 0
             for artist_data in temp_songs_dict.get('track').get("artists"):
-                yield artist(artist_data, temp_songs_dict)
+                #yield artist(artist_data, temp_songs_dict)
+                #yield
+                #yield artist(copy.deepcopy(temp_songs_dict), count)
+                yield artist(copy.deepcopy(temp_songs_dict), count)
+                count += 1
