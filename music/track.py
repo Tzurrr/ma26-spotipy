a2fc2d68778bb02295ee2ae0301a2c021c97178d
Tzurrr <zur001@gmail.com>
2022-02-09 18:58:44 +0200

    ctrl + shift + L

--- music/track.py
+++ music/track.py
@@ -3,8 +3,8 @@ from exeptions.spotipy_exeptions import *
 
 class track:
     track = {}
+
     def __init__(self, track: dict):
         if self.track != {}:
             raise CantInsertTwoTracksIntoOneExeption("Cant Insert Two Tracks Into One")
-        else:
-            self.track = track
+        self.track = track
