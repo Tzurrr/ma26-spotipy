2061a65288fe1c283556c63c5fbfed7435905110
Tzurrr <zur001@gmail.com>
2022-02-09 14:16:01 +0200

    changed the logics for artist class

--- music/artist.py
+++ music/artist.py
@@ -1,10 +1,14 @@
+import copy
+from music.album import Album
 class artist:
     artist_data = {}
     albums = []
+    def __init__(self, artist: dict, overalldata:dict):
+        self.artist_data["artist_id"] = artist.get("id")
+        self.artist_data["artist_name"] = artist.get("name")
 
-    def __init__(self, artists: list, album: dict):
-        for artist in artists:
-            self.artist_data["artist_id"] = artist.get("id")
-            self.artist_data["artist_name"] = artist.get("name")
-
-        self.albums.append(album)
+        temp_album = copy.deepcopy(overalldata)
+        temp_album.get("track").pop("artists")
+        self.albums.append(Album(temp_album))
+    def get_albums(self):
+        return self.albums
