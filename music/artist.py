ab028c3e57bffc265df70e1b5330f1cffd67d79e
Tzurrr <zur001@gmail.com>
2022-02-10 16:18:36 +0200

    removed unnessescary code lines and added a logger

--- music/artist.py
+++ music/artist.py
@@ -1,5 +1,8 @@
 import copy
+import logging
+
 from music.album import Album
+logging.basicConfig(filename=r'D:\Code\Spotipy\app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 class artist:
     def __init__(self, overalldata:dict, ord=0):
@@ -9,9 +12,8 @@ class artist:
         self.artist_data["artist_id"] = overalldata.get('track').get("artists")[ord].get("id")
         self.artist_data["artist_name"] = overalldata.get('track').get("artists")[ord].get("name")
 
+        #logging.INFO("created a copy in order to not ruin the main dict")
         temp = copy.deepcopy(overalldata)
         temp.get("track").pop("artists")
-        # if overalldata not in self.used:
-        #     self.used.append(temp)
         self.albums.append(Album(temp))
 
