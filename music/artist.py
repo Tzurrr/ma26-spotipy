a588f397f89808fe1571c87625cb7aac171c5290
Tzurrr <zur001@gmail.com>
2022-02-10 15:00:48 +0200

    removed class variables and some unnessescary code and variables

--- music/artist.py
+++ music/artist.py
@@ -1,16 +1,17 @@
 import copy
 from music.album import Album
+
 class artist:
-    artist_data = {}
-    used = []
-    albums = []
-    def __init__(self, overalldata:dict, ord):
+    def __init__(self, overalldata:dict, ord=0):
+        self.artist_data = {}
+        self.albums = []
+
         self.artist_data["artist_id"] = overalldata.get('track').get("artists")[ord].get("id")
         self.artist_data["artist_name"] = overalldata.get('track').get("artists")[ord].get("name")
 
-
-        overalldata.get("track").pop("artists")
-        if overalldata not in self.used:
-            self.used.append(overalldata)
-            self.albums.append(Album(copy.deepcopy(overalldata)))
+        temp = copy.deepcopy(overalldata)
+        temp.get("track").pop("artists")
+        # if overalldata not in self.used:
+        #     self.used.append(temp)
+        self.albums.append(Album(temp))
 
