361798a85ac12a989be8ca4c3c02f0a414015204
Tzurrr <zur001@gmail.com>
2022-02-10 16:19:03 +0200

    added a logger

--- music/track.py
+++ music/track.py
@@ -1,4 +1,7 @@
+import logging
+
 from exeptions.spotipy_exeptions import *
+logging.basicConfig(filename=r'D:\Code\Spotipy\app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
 class track:
@@ -6,5 +9,6 @@ class track:
     def __init__(self, track: dict):
         self.track = {}
         if self.track != {}:
+            logging.WARN("handeled exeption")
             raise CantInsertTwoTracksIntoOneExeption("Cant Insert Two Tracks Into One")
         self.track = track
