37f9a06b960e45a75ee43da0b7a86efd1421b86f
Tzurrr <zur001@gmail.com>
2022-02-10 16:23:49 +0200

    added a line in order to get the path of the logger from the congif file

--- read/reader.py
+++ read/reader.py
@@ -7,7 +7,9 @@ from music.artist import artist
 
 with open(r"D:\Code\Spotipy\config.yaml") as a_yaml_file:
     parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
-    path = parsed_yaml_file["SONG_DIRECTORY_PATH"].get("PATH")
+    path = parsed_yaml_file["SONG_DIRECTORY_PATH"].get("DPATH")
+    #lpath = parsed_yaml_file["LOGGING_PATH"].get("LPATH")
+
 
 def read(some_key_to_filter_on=None):
     for file in os.listdir(path):
