a6e5a27c90908b2e45580de06eb2949ddabaa45b
Tzurrr <zur001@gmail.com>
2022-02-10 16:13:35 +0200

    made the code cleaner

--- Parser/parser.py
+++ Parser/parser.py
@@ -3,6 +3,7 @@ import copy
 from music.artist import artist
 from read.reader import read
 
+
 class parser:
     def parse(self):
         count = 0
@@ -19,16 +20,3 @@ class parser:
                         count += 1
             except StopIteration:
                 break
-
-
-# a = parser.parse(parser)
-# c = 0
-# while c < 3:
-#     try:
-#         b = (next(a))
-#         print(b.artist_data)
-#         artist_data = b
-#         print(b.albums[len(b.albums)-1].album_metadata)
-#         c+=1
-#     except StopIteration:
-#         break
