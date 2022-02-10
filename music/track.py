26af40da5970cf3c26740a4b2246ab16d840fef8
Tzurrr <zur001@gmail.com>
2022-02-10 15:01:29 +0200

    removed class variables

--- music/track.py
+++ music/track.py
@@ -2,9 +2,9 @@ from exeptions.spotipy_exeptions import *
 
 
 class track:
-    track = {}
 
     def __init__(self, track: dict):
+        self.track = {}
         if self.track != {}:
             raise CantInsertTwoTracksIntoOneExeption("Cant Insert Two Tracks Into One")
         self.track = track
